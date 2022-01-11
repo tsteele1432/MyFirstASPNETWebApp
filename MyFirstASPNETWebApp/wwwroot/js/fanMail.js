15dc43fa37298592a6cb9706dfae8a3806cb1d9e
tsteele1432 <tsteele842@gmail.com>
2022-01-11 15:02:11 -0700

    Add project files.

---
+++ MyFirstASPNETWebApp/wwwroot/js/fanMail.js
@@ -0,0 +1,14 @@
+﻿////$('#btnSend').addEventListener('click', function () {
+////    alert('From: ' + $('#textFrom').value
+////        + ', Subject: ' + $('#textSubject').value
+////        + ', Message: ' + $('#textMessage').value
+////        );
+////})
+
+$('#btnSend').click(function () {
+    alert('From: ' + $('#textFrom').val() +
+        ', Subject: ' + $('#textSubject').val() +
+        ', Message: ' + $('#textMessage').val());
+
+    $('#picGanderson').fadeToggle();
+})
